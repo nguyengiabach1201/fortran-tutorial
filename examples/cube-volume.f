351b2d93a6b3f133fc65d95c8eaa059465c3be94
nguyengiabach1201 <108968549+nguyengiabach1201@users.noreply.github.com>
2023-08-26 22:10:34 +0700

    Update cube-volume.f

--- examples/cube-volume.f
+++ examples/cube-volume.f
@@ -1,1 +1,15 @@
+function func(i) result(j)
+    integer, intent (in) :: i ! input
+    integer              :: j ! output
 
+    j = i**3
+end function
+
+program main
+    implicit none
+    integer :: i
+    integer :: func
+
+    i = 3
+    print *, "Volume of cube with size", i, "is", func(i)
+end program
